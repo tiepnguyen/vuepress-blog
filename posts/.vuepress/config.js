1251e6573dc37df72a095c0702e3e336e0e53755
Tiep <tiep@tiep.info>
2019-04-24 18:56:56 +0700

    remove clean-url plugin

--- posts/.vuepress/config.js
+++ posts/.vuepress/config.js
@@ -2,7 +2,6 @@ module.exports = {
   title: 'My Vuepress Blog',
   description: 'A blog that powered by Vue',
   dest: './dist',
-  plugins: ['clean-urls'],
   themeConfig: {
     logo: '/img/logo.png',
     repo: 'https://github.com/tiepnguyen/vuepress-blog',
